6d1eedb9dad14642c830539e6b100efc8091e88e
Mohammad1724 <129998091+Mohammad1724@users.noreply.github.com>
2025-11-27 06:03:28 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,9 +9,11 @@ echo "Installing/Updating MRM Manager..."
 mkdir -p "$INSTALL_DIR"
 
 # Download Modules
+# اضافه شدن دانلود theme.sh
 curl -s -o "$INSTALL_DIR/utils.sh" "$REPO_URL/utils.sh"
 curl -s -o "$INSTALL_DIR/ssl.sh" "$REPO_URL/ssl.sh"
 curl -s -o "$INSTALL_DIR/node.sh" "$REPO_URL/node.sh"
+curl -s -o "$INSTALL_DIR/theme.sh" "$REPO_URL/theme.sh"
 curl -s -o "$INSTALL_DIR/main.sh" "$REPO_URL/main.sh"
 
 # Make executable
